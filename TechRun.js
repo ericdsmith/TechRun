70ac7eb3d8bf70c6577e7e0d579df895553bbccf
ericdsmith <tech.ericsmith@gmail.com>
2016-04-16 12:04:29 -0400

    Names added.

---
+++ TechRun.js
@@ -0,0 +1,25 @@
+//Tech Run
+
+
+
+var hrr16 = {
+	name: 'Daniel Ramos',
+<<<<<<< HEAD
+	name: 'Eric Smith',
+  name: 'Nick Randall',
+  name: 'Neekon Etemad',
+  name: 'Me Cruz',
+  nam   '  son',
+  name: 'Eduard Bardaji',
+  name: 'Josh Summerhays',
+  name: 'Timothy Woo',
+  name: 'Chris Weaver'  ,
+  name: 'Leo',
+  name: 'I can type now - Gabe'
+
+
+
+}
+=======
+}
+>>>>>>> 177c822729fc08a5c4ac2d2f02474eeff75ece34
